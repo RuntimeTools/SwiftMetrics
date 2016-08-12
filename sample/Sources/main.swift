59aa42501f4e74e400078f68d860e8e4586a449b
mattcolegate <colegate@uk.ibm.com>
2016-08-12 15:53:39 +0000

    Works on Linux

--- sample/Sources/main.swift
+++ sample/Sources/main.swift
@@ -5,7 +5,7 @@ print("Initialising SwiftMetrics class")
 let sm = try SwiftMetrics()
 
 print("Initialisation successful - starting SwiftMetrics")
-//sm.spath(path: "/vagrant/deploy/plugins")
+sm.spath(path: "/home/vagrant/SwiftMetrics/sample/.build/debug")
 sm.start()
 
 print ("Startup successful - press any key to stop")
