35bb09a0fe430094f6381c77982e90d4fb01e782
Matthew Colegate <colegate@uk.ibm.com>
2017-01-06 10:21:59 +0000

    Establish communications with Auto-Scale service

--- Package.swift
+++ Package.swift
@@ -7,9 +7,11 @@ let package = Package(
         Target(name: "Swift-BlueMix-Autoscaling-Agent", dependencies: ["SwiftMetrics"])
     ],
     dependencies: [
-        .Package(url: "https://github.com/RuntimeTools/omr-agentcore.git", majorVersion: 3),
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1),
-        .Package(url: "https://github.com/IBM-Bluemix/cf-deployment-tracker-client-swift.git", majorVersion: 0)
-    ]
-)
-
+      .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1),
+      .Package(url: "https://github.com/mattcolegate/Kitura-Request.git", majorVersion: 0),
+      .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1),
+      .Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", majorVersion: 1),
+      .Package(url: "https://github.com/RuntimeTools/omr-agentcore.git", majorVersion: 3),
+      .Package(url: "https://github.com/IBM-Bluemix/cf-deployment-tracker-client-swift.git", majorVersion: 0) 
+    ],
+    exclude: ["Makefile", "Package-Builder"])
--- Sources/Swift-BlueMix-Autoscaling-Agent/Controller.swift
+++ Sources/Swift-BlueMix-Autoscaling-Agent/Controller.swift
@@ -15,14 +15,230 @@
 **/
 
 import Kitura
+import KituraRequest
 import SwiftyJSON
 import LoggerAPI
 import CloudFoundryEnv
+import SwiftMetrics
+import Foundation
+
+fileprivate struct HttpStats {
+	fileprivate var count: Float = 0
+	fileprivate var duration: Float = 0
+	fileprivate var average: Float = 0
+}
+
+fileprivate struct MemoryStats {
+	fileprivate var count: Float = 0
+	fileprivate var sum: Float = 0
+	fileprivate var average: Float = 0
+}
+
+fileprivate struct CPUStats {
+	fileprivate var count: Float = 0
+	fileprivate var sum: Float = 0
+	fileprivate var average: Float = 0
+}
+
+fileprivate struct ThroughputStats {
+	fileprivate var duration: Double = 0
+	fileprivate var lastCalculateTime: Double = NSDate().timeIntervalSince1970
+	fileprivate var requestCount: Double = 0
+	fileprivate var throughput: Double = 0
+}
+
+fileprivate struct Metrics {
+	fileprivate var httpStats: HttpStats = HttpStats()
+	fileprivate var memoryStats: MemoryStats = MemoryStats()
+	fileprivate var cpuStats: CPUStats = CPUStats()
+	fileprivate var throughputStats: ThroughputStats = ThroughputStats()
+}
+
+fileprivate struct AverageMetrics {
+	fileprivate var responseTime: Float = 0
+	fileprivate var memory: Float = 0
+	fileprivate var cpu: Float = 0
+	fileprivate var throughput : Double = 0
+}
+
+public class AutoScalar {
+
+	var reportIntervalID: Int = 0
+	var refreshIntervalID: Int = 0
+	var reportInterval: Int = 30000
+	var refreshInterval: Int = 60000
+	var isAgentEnabled: Bool = true
+	var enabledMetrics: [String] = []
+	let autoScalingRegex = "Auto(.*)Scaling"
+	let sm: SwiftMetrics
+	let monitor: SwiftMonitor
+	fileprivate var metrics: Metrics = Metrics()
+
+	public init(metricsToEnable: [String]) throws{
+		enabledMetrics = metricsToEnable
+		sm = try SwiftMetrics()
+		monitor = sm.monitor()
+		self.setMonitors()
+		self.startReport()
+		self.notifyStatus()
+	}
+
+	public convenience init() throws {
+		try self.init(metricsToEnable: ["CPU", "Memory", "Throughput"])
+	}
+
+	private func setMonitors() {
+		monitor.on({(mem: MemData) -> () in
+			self.metrics.memoryStats.count += 1
+			self.metrics.memoryStats.sum += Float(mem.totalRAMUsed)
+		})
+		monitor.on({(cpu: CPUData) -> () in
+			self.metrics.cpuStats.count += 1
+      			self.metrics.cpuStats.sum += cpu.percentUsedByApplication * 100;
+		})
+	}
+
+	private func startReport() {
+		do {
+			guard let autoScalingService = try CloudFoundryEnv.getAppEnv().getServiceCreds(spec: autoScalingRegex) else {
+				print("[Auto-Scaling Agent] Please bind auto-scaling service!")
+				return
+			}
+			//report thread starts here
+			if (!isAgentEnabled) {
+				print("[Auto-Scaling Agent] Agent is disabled by server")
+				print("\(autoScalingService)")
+				return
+			}
+			let metricsToSend = calculateAverageMetrics()
+			_ = constructSendObject(metricsToSend: metricsToSend)
+			//sendMetrics(sendObject)
+			//report thread ends here
+		} catch {
+			print("[Auto-Scaling Agent] Unable to determine if the auto-scaling service is bound!")
+			return
+		}
+		//heartbeat thread starts here
+			//refreshHeartBeat()
+		//heartbeat thread ends here
+	}
+
+	private func calculateAverageMetrics() ->  AverageMetrics {
+		metrics.httpStats.average = (metrics.httpStats.duration > 0 && metrics.httpStats.count > 0) ? (metrics.httpStats.duration / metrics.httpStats.count) : 0.0
+		metrics.httpStats.count = 0;
+    		metrics.httpStats.duration = 0;
+
+    		metrics.memoryStats.average = (metrics.memoryStats.sum > 0 && metrics.memoryStats.count > 0) ? (metrics.memoryStats.sum / metrics.memoryStats.count) : metrics.memoryStats.average;
+		metrics.memoryStats.count = 0;
+		metrics.memoryStats.sum = 0;
+
+		metrics.cpuStats.average = (metrics.cpuStats.sum > 0 && metrics.cpuStats.count > 0) ? (metrics.cpuStats.sum / metrics.cpuStats.count) : metrics.cpuStats.average;
+		metrics.cpuStats.count = 0;
+		metrics.cpuStats.sum = 0;
+
+		if (metrics.throughputStats.requestCount > 0) {
+			let currentTime = NSDate().timeIntervalSince1970
+			let duration = currentTime - metrics.throughputStats.lastCalculateTime
+			metrics.throughputStats.throughput = metrics.throughputStats.requestCount / (duration / 1000)
+			metrics.throughputStats.lastCalculateTime = currentTime
+			metrics.throughputStats.duration = duration
+		} else {
+			metrics.throughputStats.throughput = 0
+			metrics.throughputStats.duration = 0
+		}
+		metrics.throughputStats.requestCount = 0
+
+		return AverageMetrics(responseTime: metrics.httpStats.average,
+					memory: metrics.memoryStats.average,
+					cpu: metrics.cpuStats.average,
+					throughput: metrics.throughputStats.throughput
+		)
+	}
+
+	private func createMetricObject() {
+	}
+
+	private func constructSendObject(metricsToSend: AverageMetrics) {
+		
+		//for metric in enabledMetrics {
+			//switch (metric) {
+			//	case "CPU":
+			//		print("")
+			//}
+		//}	
+	}
+
+	private func notifyStatus() {
+		var serviceEnv: [String:Any] = [:]
+		var agentUsername = "", agentPassword = "", appID = ""
+		do {
+			guard let autoScalingService = try CloudFoundryEnv.getAppEnv().getServiceCreds(spec: autoScalingRegex) else {
+				print("[Auto-Scaling Agent] notifyStatus:serviceEnv is not found or empty")
+				return
+			}
+			serviceEnv = autoScalingService
+			guard let aU = autoScalingService["agentUsername"] else {
+				print("[Auto-Scaling Agent] notifyStatus:serviceEnv.agentUsername is not found or empty")
+				return
+			}
+			agentUsername = aU as! String
+			guard let ap = autoScalingService["agentPassword"] else {
+				print("[Auto-Scaling Agent] notifyStatus:serviceEnv.agentPassword is not found or empty")
+				return
+			}
+			agentPassword = ap as! String
+			guard let aI = autoScalingService["app_id"] else {
+				print("[Auto-Scaling Agent] notifyStatus:serviceEnv.app_id is not found or empty")
+				return
+			}
+			appID = aI as! String
+		} catch {
+			print("[Auto-Scaling Agent] notifyStatus:Unable to determine if the auto-scaling service is bound!")
+			return
+		}
+
+		let host = serviceEnv["url"]!
+		let auth = "\(agentUsername):\(agentPassword)"
+		Log.info("[Auto-scaling Agent] Authorisation: \(auth)")
+		let authorization = Data(auth.utf8).base64EncodedString()
+		let notifyStatusPath = "\(host):443/services/agent/status/\(appID)"
+		Log.info("[Auto-scaling Agent] Attempting notifyStatus request to \(notifyStatusPath)")
+		KituraRequest.request(.put,
+				notifyStatusPath,
+				headers: ["Authorization":"Basic \(authorization)"]
+				).response {
+			request, response, data, error in
+				Log.info("[Auto-scaling Agent] notifyStatus:Request: \(request!)")
+				Log.info("[Auto-scaling Agent] notifyStatus:Response: \(response!)")
+				Log.info("[Auto-scaling Agent] notifyStatus:Data: \(data)")
+				Log.info("[Auto-scaling Agent] notifyStatus:Error: \(error)")
+		}
+
+		let serviceID = serviceEnv["service_id"]!
+		let refreshConfigPath = "\(host):443/v1/agent/config/\(serviceID)/\(appID)?appType=nodejs"
+		Log.info("[Auto-scaling Agent] Attempting requestConfig request to \(refreshConfigPath)")
+		KituraRequest.request(.get,
+				refreshConfigPath,
+				headers: ["Content-Type":"application/json", "Authorization":"Basic \(authorization)"]
+				).response {
+			request, response, data, error in
+				Log.info("[Auto-scaling Agent] requestConfig:Request: \(request!)")
+				Log.info("[Auto-scaling Agent] requestConfig:Response: \(response!)")
+				Log.info("[Auto-scaling Agent] requestConfig:Data: \(data!)")
+				Log.info("[Auto-scaling Agent] requestConfig:Error: \(error)")
+
+				Log.info("[Auto-scaling Agent] requestConfig:Body: \(String(data: data!, encoding: .utf8))")
+		}
+		
+	}
+		
+}
 
 public class Controller {
 
   let router: Router
   let appEnv: AppEnv
+  let asa   : AutoScalar?
 
   var port: Int {
     get { return appEnv.port }
@@ -34,6 +250,15 @@ public class Controller {
 
   init() throws {
     appEnv = try CloudFoundryEnv.getAppEnv()
+
+    if (!appEnv.isLocal) {
+	Log.info("[Auto-scaling Agent] Remote connection - starting agent")
+	asa = try AutoScalar()
+	
+    } else {
+	Log.info("[Auto-scaling Agent] Local connection - not starting")
+	asa = nil
+    }
 
     // All web apps need a Router instance to define routes
     router = Router()
@@ -52,6 +277,7 @@ public class Controller {
 
     // JSON Get request
     router.get("/autoparams", handler: getAutoParams)
+
   }
 
   public func getHello(request: RouterRequest, response: RouterResponse, next: @escaping () -> Void) throws {
