b38a714434224d430da334863c00e51600429d1a
rolivieri <roliv@us.ibm.com>
2017-01-17 16:33:35 -0600

    Refactoring Package.swift

--- Package.swift
+++ Package.swift
@@ -6,9 +6,9 @@ let package = Package(
 	Target(name: "SwiftMetricsKitura", dependencies: ["SwiftMetrics"])
     ],
     dependencies: [
-      .Package(url: "https://github.com/IBM-Swift/Kitura-Request.git", majorVersion: 0),
+      .Package(url: "https://github.com/IBM-Swift/Kitura-Request.git", majorVersion: 0, minor: 6),
       .Package(url: "https://github.com/RuntimeTools/omr-agentcore.git", majorVersion: 3),
       .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 15),
-      .Package(url: "https://github.com/IBM-Bluemix/cf-deployment-tracker-client-swift.git", majorVersion: 1) 
+      .Package(url: "https://github.com/IBM-Swift/Swift-cfenv", majorVersion: 2, minor: 0)
     ]
 )
