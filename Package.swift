56bfd067f77240cecc86ba2b8bb7cfe7cffbc5ae
mattcolegate <colegate@uk.ibm.com>
2017-01-17 17:53:52 +0000

    Update dependency versions

--- Package.swift
+++ Package.swift
@@ -9,6 +9,6 @@ let package = Package(
       .Package(url: "https://github.com/IBM-Swift/Kitura-Request.git", majorVersion: 0),
       .Package(url: "https://github.com/RuntimeTools/omr-agentcore.git", majorVersion: 3),
       .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 15),
-      .Package(url: "https://github.com/IBM-Bluemix/cf-deployment-tracker-client-swift.git", majorVersion: 0) 
+      .Package(url: "https://github.com/IBM-Bluemix/cf-deployment-tracker-client-swift.git", majorVersion: 1) 
     ]
 )
