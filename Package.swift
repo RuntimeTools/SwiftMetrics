ca275b086fcf012b7307827575642515e0496bd8
corbint@uk.ibm.com <corbint@uk.ibm.com>
2017-01-27 09:47:23 +0000

    merge all code together

--- Package.swift
+++ Package.swift
@@ -2,8 +2,15 @@ import PackageDescription
 
 let package = Package(
   name: "SwiftMetrics",
+  targets: [
+      Target(name: "SwiftMetricsKitura", dependencies: ["SwiftMetrics"]),
+      Target(name: "SwiftMetricsBluemix", dependencies: ["SwiftMetricsKitura"])
+    ],  
   dependencies: [
     .Package(url: "https://github.com/RuntimeTools/omr-agentcore.git", majorVersion: 3),
+    .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 1, minor: 5),
+    .Package(url: "https://github.com/IBM-Swift/Kitura-Request.git", majorVersion: 0, minor: 6),
+    .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 15),
     .Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", majorVersion: 2, minor: 0)
   ]
 )
