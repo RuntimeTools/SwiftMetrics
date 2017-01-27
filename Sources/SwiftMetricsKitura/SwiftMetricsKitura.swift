c98037d9134293453b641ee1a1d050e7bda8db8b
corbint@uk.ibm.com <corbint@uk.ibm.com>
2017-01-27 09:57:59 +0000

    merge all code together

---
+++ Sources/SwiftMetricsKitura/SwiftMetricsKitura.swift
@@ -0,0 +1,91 @@
+import KituraNet
+import SwiftMetrics
+import Foundation
+import Dispatch
+
+public struct HTTPData: SMData {
+  public let timeOfRequest: Int
+  public let url: String
+  public let duration: Double
+  public let statusCode: HTTPStatusCode?
+  public let requestMethod: String
+}
+
+// This structure stores a request and its associated request time
+struct requests {
+    var request: ServerRequest
+    var requestTime: Double
+}
+
+// Array of requests for parsing later
+var requestStore = [requests]()
+
+private class HttpMonitor: ServerMonitor {
+    private let sM: SwiftMetrics
+    let queue = DispatchQueue(label: "requestStoreQueue")
+
+    init(swiftMetricsInstance: SwiftMetrics) {
+        self.sM = swiftMetricsInstance
+    }
+
+    // Calculate timeInMilliseconds since epoch
+    var timeIntervalSince1970MilliSeconds: Double {
+        return NSDate().timeIntervalSince1970 * 1000
+    }
+
+    // This function is called from Kitura.net when an http request starts
+    public func started(request: ServerRequest, response: ServerResponse) {
+        queue.async {
+            // Only keep 1000 unprocessed calls to conserve memory (this is a guess estimate value)
+            if (requestStore.count > 1000) {
+                requestStore.removeFirst()
+            }
+            requestStore.append(requests(request: request, requestTime: self.timeIntervalSince1970MilliSeconds))
+        }
+    }
+
+    // This function is called from Kitura.net when an http request finishes
+    public func finished(request: ServerRequest?, response: ServerResponse) {
+        if let request = request {
+            queue.async {
+                for (index,req) in requestStore.enumerated() {
+                    if request === req.request {
+                       self.sM.emitData(HTTPData(timeOfRequest:Int(req.requestTime), url:req.request.urlURL.absoluteString, duration:(self.timeIntervalSince1970MilliSeconds - req.requestTime), statusCode:response.statusCode, requestMethod:req.request.method))
+                       requestStore.remove(at:index)
+                    }
+                }
+            }
+        }
+    }
+}
+
+public typealias httpClosure = (HTTPData) -> ()
+
+public extension SwiftMonitor.EventEmitter {
+    static var httpObservers: [httpClosure] = []
+
+    static func publish(data: HTTPData) {
+        for process in httpObservers {
+            process(data)
+        }
+    }
+
+    static func subscribe(callback: @escaping httpClosure) {
+        httpObservers.append(callback)
+    }
+}
+
+public class SwiftMetricsKitura {
+
+    public init(swiftMetricsInstance: SwiftMetrics){
+        Monitor.delegate = HttpMonitor(swiftMetricsInstance: swiftMetricsInstance)
+    }
+
+    public func enable(swiftMetricsInstance: SwiftMetrics) {
+        Monitor.delegate = HttpMonitor(swiftMetricsInstance: swiftMetricsInstance)
+    }
+
+    public func disable() {
+        Monitor.delegate = nil
+    }
+}
