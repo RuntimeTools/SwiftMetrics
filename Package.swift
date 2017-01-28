fc49a7a100b651382ef9798652d9cb70e5f3de24
Jennifer Oliver <king@us.ibm.com>
2017-01-28 11:43:29 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     ],  
   dependencies: [
     .Package(url: "https://github.com/RuntimeTools/omr-agentcore.git", majorVersion: 3),
-    .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 1, minor: 5),
+    .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 1, minor: 6),
     .Package(url: "https://github.com/IBM-Swift/Kitura-Request.git", majorVersion: 0, minor: 6),
     .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 15),
     .Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", majorVersion: 2, minor: 0)
