9ec8e65d86c4bb168ad29d80b06c1af4f8acf8f3
Matthew Colegate <colegate@uk.ibm.com>
2017-02-07 16:55:55 +0000

    Update copyright headers

--- public/js/httpThroughPutChart.js
+++ public/js/httpThroughPutChart.js
@@ -1,9 +1,27 @@
+/*******************************************************************************
+ * Copyright 2017 IBM Corp.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
+
+// Line chart for displaying average http request response time at a given point in time
+
+// data storage
+var httpRate = [];
+
 //set the scale dimensions to the size of the graph
 var httpTP_xScale = d3.time.scale().range([0, graphWidth]);
 var httpTP_yScale = d3.scale.linear().range([tallerGraphHeight, 0]);
-
-// graph data storage
-var httpRate = [];
 
 // x axis format
 var httpTP_xAxis = d3.svg.axis().scale(httpTP_xScale)
