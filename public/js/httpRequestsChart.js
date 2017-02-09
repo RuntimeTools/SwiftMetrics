0e5ba0a388c82724ca7a9f51060865aaaaed245b
julienarnold <Julien.Arnold@uk.ibm.com>
2017-02-09 13:53:21 +0000

    refactoring

--- public/js/httpRequestsChart.js
+++ public/js/httpRequestsChart.js
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2017 IBM Corp.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -46,7 +46,7 @@ var httpline = d3.svg.line()
         return http_yScale(d.duration);
     });
 
-var httpSVG = d3.select("#httpDiv1")
+var httpSVG = d3.select("#httpResponseDiv")
     .append("svg")
     .attr("width", httpCanvasWidth)
     .attr("height", canvasHeight)
@@ -111,7 +111,7 @@ function updateHttpData() {
                     httpAverages[d.url] = [(averageResponseTime * hits + parseFloat(d.duration))/(hits + 1), hits + 1]
                 } else {
                     httpAverages[d.url] = [parseFloat(d.duration), 1]
-                } 
+                }
             }
         }
 
@@ -212,4 +212,3 @@ function resizeHttpChart() {
 updateHttpData()
 
 setInterval(updateHttpData, 2000);
-
