69de9bfa56cdb83c6dd0a31bcce23ff45d8af5d9
tobespc <corbint@uk.ibm.com>
2017-03-09 10:36:26 +0000

    Websockets

--- Sources/SwiftMetricsDash/SwiftMetricsDash.swift
+++ Sources/SwiftMetricsDash/SwiftMetricsDash.swift
@@ -117,7 +117,7 @@ class SwiftMetricsService: WebSocketService {
         monitor.on(sendMEM)
         monitor.on(storeHTTP)
         gethttpRequest()
-        gethttpURLs()
+      //  gethttpURLs()
   }
 
 
@@ -215,7 +215,7 @@ class SwiftMetricsService: WebSocketService {
     }
 
     public func storeHTTP(myhttp: HTTPData) {
-    	httpQueue.sync {
+    	httpQueue.async {
             if self.httpAggregateData.total == 0 {
                 self.httpAggregateData.total = 1
                 self.httpAggregateData.timeOfRequest = myhttp.timeOfRequest
@@ -233,7 +233,7 @@ class SwiftMetricsService: WebSocketService {
               }
             }
         }
-        httpURLsQueue.sync {
+        httpURLsQueue.async {
             let urlTuple = self.httpURLData[myhttp.url]
             if(urlTuple != nil) {
                 let averageResponseTime = urlTuple!.0
@@ -248,7 +248,7 @@ class SwiftMetricsService: WebSocketService {
 
     func gethttpRequest()  {
         sleep(UInt32(2))
-        httpQueue.sync {
+        httpQueue.async {
             do {
                 if self.httpAggregateData.total > 0 {
                     let httpLine = JSON([
@@ -268,14 +268,14 @@ class SwiftMetricsService: WebSocketService {
                 }
             }
         }
-        DispatchQueue.global(qos: .background).sync {
+        DispatchQueue.global(qos: .background).async {
           self.gethttpRequest()
         }
     }
 
     func gethttpURLs() {
         sleep(UInt32(2))
-        httpURLsQueue.sync {
+        httpURLsQueue.async {
             var responseData:[JSON] = []
             for (key, value) in self.httpURLData {
                 let json = JSON(["url":key, "averageResponseTime": value.0])
@@ -294,7 +294,7 @@ class SwiftMetricsService: WebSocketService {
                 }
             }
         }
-        DispatchQueue.global(qos: .background).sync {
+        DispatchQueue.global(qos: .background).async {
           self.gethttpURLs()
         }
     }
