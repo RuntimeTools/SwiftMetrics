a0f15ba7c8e13a9663881c1ce48f3e0f3c61a3eb
sjanuary <sjanuary@uk.ibm.com>
2017-03-13 11:56:30 +0000

    Fix bug in drawing http requests graph

--- public/js/httpRequestsChart.js
+++ public/js/httpRequestsChart.js
@@ -112,6 +112,11 @@ function updateHttpData(httpRequest) {
             httpChartPlaceholder.attr("visibility", "hidden");
         }
 
+        httpRequestData.longest = parseFloat(httpRequestData.longest)
+        httpRequestData.average = parseFloat(httpRequestData.average)
+        httpRequestData.time = parseInt(httpRequestData.time)
+        httpRequestData.total = parseInt(httpRequestData.total)
+
         httpData.push(httpRequestData);
 
         // Only keep 30 minutes or 2000 items of data
@@ -159,10 +164,10 @@ function updateHttpData(httpRequest) {
                     } else {
                         return d.total
                          + " requests\n average duration = "
-                         + d3.format(".2s")(d.average)
-                         + "ms\n longest duration = "
-                         +  d3.format(".2s")(d.longest)
-                         + "ms for URL: " + d.url;
+                         + d3.format(".2s")(d.average/1000)
+                         + "s\n longest duration = "
+                         +  d3.format(".2s")(d.longest/1000)
+                         + "s for URL: " + d.url;
                     }
                 });
         }
