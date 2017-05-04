0ff422944b13efa3d8179a96e9690d5640893d71
sjanuary <sjanuary@uk.ibm.com>
2017-05-04 11:13:17 +0100

    try kitura ws 0.8

--- Package.swift
+++ Package.swift
@@ -42,8 +42,8 @@ let package = Package(
    ],
   dependencies: [
     .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 7),
-    .Package(url: "https://github.com/IBM-Swift/Kitura-WebSocket.git", majorVersion: 0, minor: 7),
-    .Package(url: "https://github.com/IBM-Swift/Kitura-Request.git", majorVersion: 0, minor: 7),
+    .Package(url: "https://github.com/IBM-Swift/Kitura-WebSocket.git", majorVersion: 0, minor: 8),
+    .Package(url: "https://github.com/IBM-Swift/Kitura-Request.git", majorVersion: 0, minor: 8),
     .Package(url: "https://github.com/IBM-Swift/CloudConfiguration.git", majorVersion: 1)
   ],
    exclude: [ "Sources/agentcore/ibmras/common/port/aix",
