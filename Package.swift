5fc7f7de91c8ee1f4c101f1fba1e4e4f388ef1e4
mattcolegate <colegate@uk.ibm.com>
2017-05-19 10:43:30 +0100

    Don't need HeliumLogger, just the Logger API

--- Package.swift
+++ Package.swift
@@ -44,7 +44,6 @@ let package = Package(
   dependencies: [
     .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 6),
     .Package(url: "https://github.com/IBM-Swift/Kitura-WebSocket.git", majorVersion: 0, minor: 7),
-    .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 6),
     .Package(url: "https://github.com/IBM-Swift/Kitura-Request.git", majorVersion: 0, minor: 7),
     .Package(url: "https://github.com/IBM-Swift/CloudConfiguration.git", majorVersion: 1)
   ],
