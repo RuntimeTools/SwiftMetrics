30ae23c95379cde63c09febaf06305df93281a70
sjanuary <sjanuary@uk.ibm.com>
2017-05-22 10:59:07 +0100

    Add debug to diagnose test crash on Travis

--- Sources/SwiftMetrics/SwiftMonitor.swift
+++ Sources/SwiftMetrics/SwiftMonitor.swift
@@ -183,6 +183,7 @@ public class SwiftMonitor {
           number.of.processors=2
           command.line=/home/exampleuser/SwiftMetrics/sample/.build/debug/test
           */
+      print("formatOSEnv message = \(message)")
       let values = message.components(separatedBy: "\n")
       var env: [ String : String ] = [:]
       for value in values {
