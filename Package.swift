9262f1786e621263f5fa82df8b1f4e1649c11836
Ricardo Olivieri <rolivieri@users.noreply.github.com>
2017-09-11 04:40:12 -0500

    A few updates (mainly around CloudConfiguration). (#138)

--- Package.swift
+++ Package.swift
@@ -25,42 +25,51 @@ import PackageDescription
 let package = Package(
   name: "SwiftMetrics",
   targets: [
-      Target(name: "SwiftMetrics", dependencies: [.Target(name: "agentcore"),
-                                                   .Target(name: "cpuplugin"),
-                                                   .Target(name: "envplugin"),
-                                                   .Target(name: "memplugin"),
-                                                   .Target(name: "hcapiplugin")]),
+      Target(name: "SwiftMetrics",
+        dependencies: [
+          .Target(name: "agentcore"),
+          .Target(name: "cpuplugin"),
+          .Target(name: "envplugin"),
+          .Target(name: "memplugin"),
+          .Target(name: "hcapiplugin")
+        ]
+      ),
       Target(name: "SwiftMetricsKitura", dependencies: ["SwiftMetrics"]),
       Target(name: "SwiftBAMDC", dependencies: ["SwiftMetricsKitura"]),
-      Target(name: "SwiftMetricsBluemix", dependencies: ["SwiftMetricsKitura","SwiftBAMDC"]),
+      Target(name: "SwiftMetricsBluemix", dependencies: ["SwiftMetricsKitura", "SwiftBAMDC"]),
       Target(name: "SwiftMetricsDash", dependencies: ["SwiftMetricsBluemix"]),
-      Target(name: "mqttplugin", dependencies: [.Target(name: "paho"),
-                                                   .Target(name: "agentcore")]),
-      Target(name: "cpuplugin", dependencies: [.Target(name: "agentcore")]),
-      Target(name: "envplugin", dependencies: [.Target(name: "agentcore")]),
-      Target(name: "memplugin", dependencies: [.Target(name: "agentcore")]),
+      Target(name: "mqttplugin", 
+        dependencies: [
+            .Target(name: "paho"),
+            .Target(name: "agentcore")
+        ]
+      ),
+      Target(name: "cpuplugin", dependencies: [ .Target(name: "agentcore") ]),
+      Target(name: "envplugin", dependencies: [ .Target(name: "agentcore") ]),
+      Target(name: "memplugin", dependencies: [ .Target(name: "agentcore") ]),
       Target(name: "hcapiplugin", dependencies: [.Target(name: "agentcore")])
    ],
   dependencies: [
-    .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 6),
-    .Package(url: "https://github.com/IBM-Swift/Kitura-WebSocket.git", majorVersion: 0, minor: 7),
-    .Package(url: "https://github.com/IBM-Swift/Kitura-Request.git", majorVersion: 0, minor: 7),
-    .Package(url: "https://github.com/IBM-Swift/CloudConfiguration.git", majorVersion: 1)
+    .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 7),
+    .Package(url: "https://github.com/IBM-Swift/Kitura-WebSocket.git", majorVersion: 0, minor: 9),
+    .Package(url: "https://github.com/IBM-Swift/Kitura-Request.git", majorVersion: 0, minor: 8),
+    .Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", majorVersion: 4)
   ],
-   exclude: [ "Sources/agentcore/ibmras/common/port/aix",
-              "Sources/agentcore/ibmras/common/port/windows",
-              "Sources/agentcore/ibmras/common/data",
-              "Sources/agentcore/ibmras/common/util/memUtils.cpp",
-              "Sources/ostreamplugin",
-              "Sources/paho/Windows Build",
-              "Sources/paho/build",
-              "Sources/paho/doc",
-              "Sources/paho/test",
-              "Sources/paho/src/MQTTClient.c",
-              "Sources/paho/src/MQTTVersion.c",
-              "Sources/paho/src/SSLSocket.c",
-              "Sources/paho/src/samples",
-              excludePortDir
-   ]
+   exclude: [
+    "Sources/agentcore/ibmras/common/port/aix",
+    "Sources/agentcore/ibmras/common/port/windows",
+    "Sources/agentcore/ibmras/common/data",
+    "Sources/agentcore/ibmras/common/util/memUtils.cpp",
+    "Sources/ostreamplugin",
+    "Sources/paho/Windows Build",
+    "Sources/paho/build",
+    "Sources/paho/doc",
+    "Sources/paho/test",
+    "Sources/paho/src/MQTTClient.c",
+    "Sources/paho/src/MQTTVersion.c",
+    "Sources/paho/src/SSLSocket.c",
+    "Sources/paho/src/samples",
+    excludePortDir
+  ]
 )
 
