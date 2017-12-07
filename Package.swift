60817537b21833b9a483895368f9149b66da542f
Kye Maloy <kyemaloy2013@gmail.com>
2017-12-07 17:48:07 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -33,7 +33,7 @@ let package = Package(
         .executable(name: "SwiftMetricsCommonSample", targets: ["SwiftMetricsCommonSample"]),
     ],
   dependencies: [
-    .package(url: "https://github.com/IBM-Swift/Kitura.git", from: "2.0.0")),
+    .package(url: "https://github.com/IBM-Swift/Kitura.git", from: "2.0.0"),
     .package(url: "https://github.com/IBM-Swift/Kitura-WebSocket.git", from: "1.0.0"),
     .package(url: "https://github.com/IBM-Swift/SwiftyRequest.git", from: "1.0.0"),
     .package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", from: "6.0.0"),
