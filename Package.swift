3b7ac26b7bf2a915e1aaf75ffb7b7ed3d982910b
Ian Partridge <i.partridge@uk.ibm.com>
2018-10-19 11:30:26 +0100

    chore: update SwiftyRequest to v2

--- Package.swift
+++ Package.swift
@@ -45,7 +45,7 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/IBM-Swift/Kitura.git", from: "2.3.0"),
     webSocketPackage,
-    .package(url: "https://github.com/IBM-Swift/SwiftyRequest.git", from: "1.0.0"),
+    .package(url: "https://github.com/IBM-Swift/SwiftyRequest.git", from: "2.0.0"),
     .package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", from: "6.0.0"),
     .package(url: "https://github.com/RuntimeTools/omr-agentcore", .exact("3.2.4-swift4")),
   ],
