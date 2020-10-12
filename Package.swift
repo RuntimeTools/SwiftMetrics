a30520ee4953865df9b7b8c1745315b9f5d54898
Mathieu Barnachon <mathieu@styleshoots.com>
2020-10-12 14:10:53 +0200

    Add `connectionTimeout` from WebSocketService. This is required by the new version of the WebSocketService protocol. Also update dependencies to the community version of Kitura (moved away from IBM-Swift).

--- Package.swift
+++ Package.swift
@@ -21,9 +21,9 @@ import Foundation
 var webSocketPackage: Package.Dependency
 
 if ProcessInfo.processInfo.environment["KITURA_NIO"] != nil {
-    webSocketPackage = .package(url: "https://github.com/IBM-Swift/Kitura-WebSocket-NIO.git", from: "2.0.0")
+    webSocketPackage = .package(url: "https://github.com/Kitura/Kitura-WebSocket-NIO.git", from: "2.1.200")
 } else {
-    webSocketPackage = .package(url: "https://github.com/IBM-Swift/Kitura-WebSocket.git", from: "2.0.0")
+    webSocketPackage = .package(url: "https://github.com/Kitura/Kitura-WebSocket.git", from: "2.1.200")
 }
 
 let package = Package(
